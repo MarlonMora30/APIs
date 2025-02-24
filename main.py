8cc16728175d96aed8d64ca960d6ebe1203eec32
MarlonMora30 <marlon.morads@gmail.com>
2025-02-23 21:14:02 -0600

    Update main.py
    Se comenta el código para un mejor entendimiento, y se remueven comentarios innecesarios.

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ from pydantic import BaseModel #add_users
 
 from typing import List #get_users
 
-#add_users
+
 
 app = FastAPI()
 
+#Lista de usuarios en memoria
 users = []
 
+#Modelo de usuario
 class User(BaseModel):
     id: int
     name: str
     email: str
 
+#Endpoint POST para agregar usuarios
 @app.post("/users/")
 async def add_user(user: User):
     users.append(user)
     return {"message": "Usuario agregado correctamente", "user": user}
 
-#get_users
+#Endpoint GET para obtener usuarios
 
 @app.get("/users/", response_model=List[User])
 async def get_users():
